35e420128a3e6e2f3524ad87d956976c6439ff83
Manoj Upadhya <70696140+manojupadhya11@users.noreply.github.com>
2023-05-15 06:13:11 +0530

    Delete 8.Program_to_find_year_is_a_leap_year_or_not.py
    Signed-off-by: Manoj Upadhya <70696140+manojupadhya11@users.noreply.github.com>

--- 8.Program_to_find_year_is_a_leap_year_or_not.py
+++
@@ -1,7 +0,0 @@
-'''program to find a given year leap year or not'''
-year = int(input("Enter the year "))
-
-if year % 400 == 0 or (year% 4 ==0 and year % 100 != 0):
-    print(year," is a Leap year")
-else:
-    print(year," is not a leap year")
